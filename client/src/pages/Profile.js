8866a43d5e3f2aaa54d6815848c7a50ffe3981a6
bigboiearnes <122842632+bigboiearnes@users.noreply.github.com>
2024-04-30 21:04:54 +0100

    Update Profile.js
    Added loading details from database for profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,8 +1,28 @@
-export default function Profile() {
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import useFetchUser from '../components/FetchUserData';
+import { useAuth } from '../components/AuthContext';
+
+export default function Database(){
+  const { username } = useParams();
+  const { user, loading, error } = useFetchUser(username);
+  const { token } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!user) {
+    return <div>User not found</div>;
+  }
 
   return (
     <div>
-      
+      <h1>{user.username}</h1>
     </div>
   );
-}
+};
